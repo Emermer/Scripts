df12893be585fe52b601120da5f0f02f5a3d89a2
Emermer <45740363+Emermer@users.noreply.github.com>
2022-10-23 21:44:33 +0000

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+﻿#! /bin/bash
 
 ### XRANDR SETTINGS ###
 xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal --output DP-3 --mode 1680x1050 --pos 1920x30 --rotate normal &
@@ -11,30 +11,36 @@ picom &
 ### NITROGEN ###
 nitrogen --restore &
 
-### KEY PRESS DELAY/RATE ###
-xset r rate 250 25 &
-
 ### POLYBAR ###
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
-### OTHER APPS (Kill & Spawn) ###
-killall xautolock &
-killall sxhkd &
-killall flameshot &
-killall nm-applet &
-killall autotiling &
+### KEY PRESS DELAY/RATE ###
+xset r rate 250 25 &
+
+### OTHER APPS ###
+killall xautolock sxhkd flameshot nm-applet autotiling &
 sleep 0.2
 xautolock -time 5 -locker "betterlockscreen -l blur" &
 sxhkd &
 flameshot &
 nm-applet &
 autotiling &
+
+### MAILSPRING ###
+if pgrep mailspring >/dev/null
+then
+    echo mailspring
+else
+    mailspring
+fi &
+
+### REDSHIFT ###
 if pgrep redshift-gtk >/dev/null
 then
     echo redshift already running
 else
     redshift-gtk -c $HOME/.config/redshift/redshift.conf
-fi
+fi &
